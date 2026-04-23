035187ef0d68badb386017183260dbd3f5fc2032
RED-Mushaf-Ali-Mir <p240582@nu.edu.pk>
2026-04-23 07:51:53 +0500

    json Creation

---
+++ shell.c
@@ -0,0 +1,8 @@
+
+
+void shell(void){    
+
+
+    do_tracer (tracee_pid);
+
+}
---
+++ src/Ptracer.c
@@ -0,0 +1,31 @@
+#include "Ptracer.h"
+
+void do_tracer (pid_t tracee_pid){
+    int  status;
+    struct user_regs_struct regs;
+
+    if (ptrace(PTRACE_ATTACH,tracee_pid , NULL , NULL ) == -1){
+        perror ("ptrace PTRACE_ATTACH");
+        return;
+    }else {
+        printf ("\nTracee Attached Successfully");
+    }
+    waitpid (tracee_pid , &status , 0);
+    if (WIFSTOPPED(status)){
+        printf ("\nChild Stopped , PID : %d " ,tracee_pid);
+        while (1){
+            if (ptrace(PTRACE_GETREGS, tracee_pid , NULL , &regs) == -1){
+                perror ("ptrace GETREGS");
+                exit(1);
+            }
+            printf ("\nTrace syscall  orig_rax = %lld ", regs.orig_rax);
+            ptrace(PTRACE_SYSCALL , tracee_pid , NULL, NULL);
+            waitpid(tracee_pid , &status , 0);
+            if (WIFEXITED (status)){
+                printf ("\nChild Exited with status %d \n" , WEXITSTATUS(status));
+                break;
+            }
+        }
+    }
+
+}

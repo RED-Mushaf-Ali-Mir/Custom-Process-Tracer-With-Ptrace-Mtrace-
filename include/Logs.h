acc9bab025c8dc2f13373556ea940b3e004154c5
RED-Mushaf-Ali-Mir <p240582@nu.edu.pk>
2026-04-23 11:51:14 +0500

    Header definitions

---
+++ include/Logs.h
@@ -0,0 +1,7 @@
+#ifndef LOGS_H
+#define LOGS_H
+//First write function definiton here then implemnt is src folder in repective file
+
+int logTrace (int sysCallNum , char* sysCallName);
+
+#endif
---
+++ include/SysCalls.h
@@ -0,0 +1,8 @@
+#ifndef SYSCALLS_H
+#define SYSCALL_H
+
+//First write function definiton here then implemnt is src folder in repective file
+
+char* sysCallName (int sysCallNum);
+
+#endif
